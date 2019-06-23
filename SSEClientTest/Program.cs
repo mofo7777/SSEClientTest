8610949d81669d305e0d98bcd3c5cdf7258c2427
mofo7777 <41967574+mofo7777@users.noreply.github.com>
2019-06-23 22:11:55 +0200

    Add files via upload

---
+++ SSEClientTest/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+using System.Diagnostics;
+
+namespace SSEClientTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            SingleSSEClientThreadManager Client = new SingleSSEClientThreadManager("http://live.meetscoresonline.com/test-sse.aspx");
+            Client.m_SSEWebClient.SSEWebClientEvent += OnSSEWebClientEvent;
+            Client.Start();
+
+            Thread.Sleep(5000);
+
+            Client.Stop();
+
+            Client.m_SSEWebClient.SSEWebClientEvent -= OnSSEWebClientEvent;
+        }
+
+        public static void OnSSEWebClientEvent(object sender, SSEWebClientEventArgs e)
+        {
+            Debug.WriteLine("Message : " + e.szMessage);
+        }
+    }
+}
---
+++ SSEClientTest/SingleSSEClientThreadManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace SSEClientTest
+{
+    class SingleSSEClientThreadManager
+    {
+        public SSEWebClient m_SSEWebClient = null;
+        private string m_szUri { get; set; }
+        private Task m_Task = null;
+
+        public SingleSSEClientThreadManager(string szUri)
+        {
+            m_szUri = szUri;
+            m_SSEWebClient = new SSEWebClient();
+        }
+
+        public bool Start()
+        {
+            if (m_Task != null)
+            {
+                Debug.WriteLine("SingleSSEClientThreadManager Start : m_Task != null");
+                return false;
+            }
+
+            m_Task = Task.Run(() => m_SSEWebClient.Start(m_szUri));
+
+            Debug.WriteLine("SingleSSEClientThreadManager Start : " + m_Task.Status.ToString());
+            Debug.WriteLine("SingleSSEClientThreadManager Start : OK");
+
+            return true;
+        }
+
+        public bool Stop()
+        {
+            if (m_Task == null)
+            {
+                Debug.WriteLine("SingleSSEClientThreadManager Stop : m_Task == null");
+                return false;
+            }
+
+            Debug.WriteLine("SingleSSEClientThreadManager Stop 1 : " + m_Task.Status.ToString());
+            m_Task.Wait();
+            Debug.WriteLine("SingleSSEClientThreadManager Stop 2 : " + m_Task.Status.ToString());
+
+            m_Task.Dispose();
+            m_Task = null;
+
+            Debug.WriteLine("SingleSSEClientThreadManager Stop : OK");
+
+            return true;
+        }
+    }
+}
